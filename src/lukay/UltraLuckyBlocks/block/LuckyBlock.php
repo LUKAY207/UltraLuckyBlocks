fcca5bc3f491662e7b839a5b523ca1fd4c2e1217
Luka <127244631+LUKAY207@users.noreply.github.com>
2024-04-05 16:36:49 +0200

    Small change

--- src/lukay/UltraLuckyBlocks/block/LuckyBlock.php
+++ src/lukay/UltraLuckyBlocks/block/LuckyBlock.php
@@ -29,49 +29,27 @@ class LuckyBlock extends Block {
 
         if ($itemKey == 0) {
             return [VanillaItems::NETHERITE_SWORD(), VanillaItems::NETHERITE_AXE(), VanillaItems::NETHERITE_PICKAXE(), VanillaItems::NETHERITE_SHOVEL(), VanillaItems::NETHERITE_HOE()];
-        }
-
-        if ($itemKey == 1) {
+        } elseif ($itemKey == 1) {
             return [VanillaItems::NETHERITE_HELMET(), VanillaItems::NETHERITE_CHESTPLATE(), VanillaItems::NETHERITE_LEGGINGS(), VanillaItems::NETHERITE_BOOTS()];
-        }
-
-        if ($itemKey == 2) {
+        } elseif ($itemKey == 2) {
             return [VanillaItems::DIAMOND_SWORD(), VanillaItems::DIAMOND_AXE(), VanillaItems::DIAMOND_PICKAXE(), VanillaItems::DIAMOND_SHOVEL(), VanillaItems::DIAMOND_HOE()];
-        }
-
-        if ($itemKey == 3) {
+        } elseif ($itemKey == 3) {
             return [VanillaItems::DIAMOND_HELMET(), VanillaItems::DIAMOND_CHESTPLATE(), VanillaItems::DIAMOND_LEGGINGS(), VanillaItems::DIAMOND_BOOTS()];
-        }
-
-        if ($itemKey == 4) {
+        } elseif ($itemKey == 4) {
             return [VanillaItems::GOLDEN_SWORD(), VanillaItems::GOLDEN_AXE(), VanillaItems::GOLDEN_PICKAXE(), VanillaItems::GOLDEN_SHOVEL(), VanillaItems::GOLDEN_HOE()];
-        }
-
-        if ($itemKey == 5) {
+        } elseif ($itemKey == 5) {
             return [VanillaItems::GOLDEN_HELMET(), VanillaItems::GOLDEN_CHESTPLATE(), VanillaItems::GOLDEN_LEGGINGS(), VanillaItems::GOLDEN_BOOTS()];
-        }
-
-        if ($itemKey == 6) {
+        } elseif ($itemKey == 6) {
             return [VanillaItems::IRON_SWORD(), VanillaItems::IRON_AXE(), VanillaItems::IRON_PICKAXE(), VanillaItems::IRON_SHOVEL(), VanillaItems::IRON_HOE()];
-        }
-
-        if ($itemKey == 7) {
+        } elseif ($itemKey == 7) {
             return [VanillaItems::IRON_HELMET(), VanillaItems::IRON_CHESTPLATE(), VanillaItems::IRON_LEGGINGS(), VanillaItems::IRON_BOOTS()];
-        }
-
-        if ($itemKey == 8) {
+        } elseif ($itemKey == 8) {
             return [VanillaItems::STONE_SWORD(), VanillaItems::STONE_AXE(), VanillaItems::STONE_PICKAXE(), VanillaItems::STONE_SHOVEL(), VanillaItems::STONE_HOE()];
-        }
-
-        if ($itemKey == 9) {
+        } elseif ($itemKey == 9) {
             return [VanillaItems::CHAINMAIL_HELMET(), VanillaItems::CHAINMAIL_CHESTPLATE(), VanillaItems::CHAINMAIL_LEGGINGS(), VanillaItems::CHAINMAIL_BOOTS()];
-        }
-
-        if ($itemKey == 10) {
+        } elseif ($itemKey == 10) {
             return [VanillaItems::WOODEN_SWORD(), VanillaItems::WOODEN_AXE(), VanillaItems::WOODEN_PICKAXE(), VanillaItems::WOODEN_SHOVEL(), VanillaItems::WOODEN_HOE()];
-        }
-
-        if ($itemKey == 11) {
+        } elseif ($itemKey == 11) {
             return [VanillaItems::LEATHER_CAP(), VanillaItems::LEATHER_TUNIC(), VanillaItems::LEATHER_PANTS(), VanillaItems::LEATHER_BOOTS()];
         }
     }
